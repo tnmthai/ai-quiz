dd518e11d159f675f1f52c93f8dce67f81a07952
Thai Tran <tnmthai@gmail.com>
2026-05-05 00:19:10 +1200

    Fix: Express 5 wildcard route syntax

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use('/api/quiz', quizRoutes);
 
 // Serve frontend
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('*', (req, res) => {
+app.get('/{*path}', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
